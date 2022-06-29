75b625430d3663635eed9a3b272e8cde0f351df6
Meifuku <39018575+meifuku@users.noreply.github.com>
2022-06-29 19:58:31 -0400

    bitsery

--- cmake/includes.cmake
+++ cmake/includes.cmake
@@ -1,2 +1,2 @@
-add_library(args INTERFACE)
-target_include_directories(args INTERFACE "${PROJECT_SOURCE_DIR}/submodules/include")
+add_library(header_only INTERFACE)
+target_include_directories(header_only INTERFACE "${PROJECT_SOURCE_DIR}/submodules/include")
